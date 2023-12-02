25e01c25178e5ad81aa874d40a85af242c013d72
Notaerium <parneix.vinh@gmail.com>
2023-12-02 22:12:16 +0100

    Animation is now smooth

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,10 +8,10 @@ let rightboxes = document.querySelectorAll(".right")
 document.addEventListener('scroll', ()=> {
     leftboxes.forEach((leftbox, index) => {
         if(leftbox.getBoundingClientRect().y < window.screen.height - 100 - leftbox.offsetHeight){
-            leftbox.classList.remove('left')
-            leftbox.classList.toggle('leftslide')
-            rightboxes[index].classList.remove('right')
-            rightboxes[index].classList.toggle('rightslide')
+            if(!leftbox.classList.contains('leftslide')){
+                leftbox.classList.add('leftslide')
+                rightboxes[index].classList.add('rightslide')
+            }
         }
     });
 })
