937331610428ed022eebe67956d5022f0d56a809
Notaerium <parneix.vinh@gmail.com>
2023-12-04 12:04:06 +0100

    Responsive done but problem of a not smooth animation on projects

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,7 +7,9 @@ let rightboxes = document.querySelectorAll(".right")
 
 document.addEventListener('scroll', ()=> {
     leftboxes.forEach((leftbox, index) => {
-        if(leftbox.getBoundingClientRect().y < window.screen.height - 100 - leftbox.offsetHeight){
+        if(leftbox.getBoundingClientRect().y < window.screen.height - 100 - leftbox.offsetHeight ||
+        rightboxes[index].getBoundingClientRect().y < window.screen.height - 100 - rightboxes[index].offsetHeight
+        ){
             if(!leftbox.classList.contains('leftslide')){
                 leftbox.classList.add('leftslide')
                 rightboxes[index].classList.add('rightslide')
@@ -15,3 +17,8 @@ document.addEventListener('scroll', ()=> {
         }
     });
 })
+
+function showMenu(){
+    let menu= document.querySelector(".menu")
+    menu.classList.toggle('display')
+}
